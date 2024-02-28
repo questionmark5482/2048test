f422c5cd45b130bcb5b75d1ac6e0139c62188e23
questionmark5482 <jiahaichenxju@163.com>
2024-02-28 20:44:14 +0800

    123

--- scripts/board.gd
+++ scripts/board.gd
@@ -1,8 +1,7 @@
 extends Node2D
+
 var blocks = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
-
 var alive = true
-
 var move_count = 0
 
 # Called when the node enters the scene tree for the first time.
@@ -11,14 +10,8 @@ func _ready():
 	debug_print_board()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	
-
 func initialize():
 	spawn_number()
-	pass
 	
 	
 func spawn_number():
@@ -55,6 +48,15 @@ func debug_print_board():
 	print(blocks[2])
 	print(blocks[3])
 
+func try_slide(dir):
+	# check if possible
+	
+	# rotate
+	
+	# slide_left()
+	
+	# rotate back
+	pass
 
 func slide_left():
 	for row_index in range(4):
@@ -78,104 +80,19 @@ func slide_left():
 		for ii in range(4 - len(new_row)):
 			new_row.append(0)
 		blocks[row_index] = new_row
-		
-func slide_right():
-	for row_index in range(4):
-		var row = blocks[row_index]
-		var non_zeros = []
-		for num in row:
-			if num != 0:
-				non_zeros.append(num)
-		# convert non_zeros into new_row. 
-		var new_row = []
-		while non_zeros:
-			var a = non_zeros.pop_back()
-			if non_zeros == []:
-				new_row.push_front(a)
-				break
-			if non_zeros[-1] == a:
-				non_zeros.pop_back()
-				new_row.push_front(a*2)
-			else:
-				new_row.push_front(a)
-		for ii in range(4 - len(new_row)):
-			new_row.push_front(0)
-		blocks[row_index] = new_row
-
-func slide_up():
-	var new_blocks = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
-	for row_index in range(4):
-		# pick a "row" according to direction
-		var row = []
-		for jj in range(4):
-			row.append(blocks[jj][row_index])
-		
-		var non_zeros = []
-		for num in row:
-			if num != 0:
-				non_zeros.append(num)
-		# convert non_zeros into new_row. 
-		var new_row = []
-		while non_zeros:
-			var a = non_zeros.pop_front()
-			if non_zeros == []:
-				new_row.push_back(a)
-				break
-			if non_zeros[0] == a:
-				non_zeros.pop_front()
-				new_row.push_back(a*2)
-			else:
-				new_row.push_back(a)
-		for ii in range(4 - len(new_row)):
-			new_row.push_back(0)
-		
-		for jj in range(4):
-			new_blocks[jj][row_index] = new_row[jj]
-	blocks = new_blocks
-	
-func slide_down():
-	var new_blocks = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
-	for row_index in range(4):
-		# pick a "row" according to direction
-		var row = []
-		for jj in range(4):
-			row.append(blocks[jj][row_index])
-		
-		var non_zeros = []
-		for num in row:
-			if num != 0:
-				non_zeros.append(num)
-		# convert non_zeros into new_row. 
-		var new_row = []
-		while non_zeros:
-			var a = non_zeros.pop_back()
-			if non_zeros == []:
-				new_row.push_front(a)
-				break
-			if non_zeros[-1] == a:
-				non_zeros.pop_back()
-				new_row.push_front(a*2)
-			else:
-				new_row.push_front(a)
-		for ii in range(4 - len(new_row)):
-			new_row.push_front(0)
-		
-		for jj in range(4):
-			new_blocks[jj][row_index] = new_row[jj]
-	blocks = new_blocks
  
 func _input(event):
 	if not alive:
 		return
 	
 	if event.is_action_released("ui_left"):
-		slide_left()
+		try_slide("left")
 	elif event.is_action_pressed("ui_right"):
-		slide_right()
+		try_slide("right")
 	elif event.is_action_pressed("ui_up"):
-		slide_up()
+		try_slide("up")
 	elif event.is_action_pressed("ui_down"):
-		slide_down()
+		try_slide("down")
 	else:
 		return
 	move_count += 1
@@ -186,3 +103,4 @@ func _input(event):
 
 func game_over():
 	print("Game Over!")
+#	alive = false
